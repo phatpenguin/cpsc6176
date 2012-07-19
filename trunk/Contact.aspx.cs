32932319915c06a48c6caae54bc32704d766cb24
sardinas_rodrigo@columbusstate.edu <sardinas_rodrigo@columbusstate.edu@6fac47d7-2e1d-cd27-4c29-69c2a8499821>
2012-07-19 22:17:02 +0000

    I used a tool called smtp4dev to test the mail feature. Ideally instead of constructing a client and passing it "localhost", you would leave this blank and add credentials in the following lines. At least that was the way i understood it.
    ============================================================
    
    the regular expression i used to validate the email address was taken from here:
    http://www.regular-expressions.info/email.html
    
    I took the second expression presented in the section titled:
    The Official Standard: RFC 2822
    
    =======================================================
    
    I still need to add more formatting to the css for this page.
    I've been working on this most of the day and I had another class and needed to stop,
    so I went ahead and committed it so that at least something would be on the server.

--- trunk/Contact.aspx.cs
+++ trunk/Contact.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Mail;
 
 public partial class Contact : System.Web.UI.Page
 {
@@ -11,4 +12,57 @@ public partial class Contact : System.Web.UI.Page
     {
 
     }
+
+    public int SendUserMail(string fromad, string toad, string body, string header, string subjectcontent)
+    {
+        int result = 0;
+        MailMessage usermail = Mailbodplain(fromad, toad, body, header, subjectcontent);
+        SmtpClient client = new SmtpClient("localhost");
+        //Add the Creddentials- use your own email id and password
+        //client.Credentials = new System.Net.NetworkCredential("rodrigo.sardinas@gmail.com", "chacha1981"); ;
+
+        //client.Host = "smtp.gmail.com";
+        //client.Port = 465;
+        //client.EnableSsl = true;
+        try
+        {
+            client.Send(usermail);
+            result = 1;
+        }
+        catch (Exception ex)
+        {
+            result = 0;
+        } // end try 
+
+        return result;
+
+    }
+
+    public MailMessage Mailbodplain(string fromad, string toad, string body, string header, string subjectcontent)
+    {
+        System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+        try
+        {
+            string from = fromad;
+            string to = toad;
+            mail.To.Add(to);
+            mail.From = new MailAddress(from, header, System.Text.Encoding.UTF8);
+            mail.Subject = subjectcontent;
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            mail.Body = body;
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return mail;
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SendUserMail(FromTextBox.Text, "cpsc6176@googlegroups.com" , MessageTextBox.Text, "Customer", "Contact from customer");
+    }
 }
+
