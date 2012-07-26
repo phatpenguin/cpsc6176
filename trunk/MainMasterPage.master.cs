6aa40f97f1b1656ae7171bf3a874b7c2baea542a
sardinas_rodrigo@columbusstate.edu <sardinas_rodrigo@columbusstate.edu@6fac47d7-2e1d-cd27-4c29-69c2a8499821>
2012-07-26 16:35:40 +0000

    Added ajax control to master page displaying elapsed time that admin has been logged in.

--- trunk/MainMasterPage.master.cs
+++ trunk/MainMasterPage.master.cs
@@ -4,15 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
 
 public partial class MainMasterPage : System.Web.UI.MasterPage
 {
+    private TimeSpan elapsed;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
     }
     protected void LoginLinkButton_Click(object sender, EventArgs e)
     {
         //TODO IF LOGGED IN text = Log Out, and welcome message is visible
+    }
+
+    protected void LoginTimer_Tick(object sender, EventArgs e)
+    {
+        if (BeginTime.Text.Equals(""))
+        {
+            elapsed = new TimeSpan();
+            BeginTime.Text = DateTime.Now.ToString();
+        }
+        elapsed = DateTime.Now - DateTime.Parse(BeginTime.Text);
+        LoginTimeLabel.Text = string.Format("{0}:{1:00}", elapsed.Minutes, elapsed.Seconds);
+        
     }
 }
