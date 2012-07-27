3117ab7c0f951454a99733400471d6d07722a042
scottwleonard@gmail.com <scottwleonard@gmail.com@6fac47d7-2e1d-cd27-4c29-69c2a8499821>
2012-07-27 22:03:09 +0000

    Added linq to sql files and updated wcfwebservice reference.  Fixed a repository conflict in the master page and web.config.

--- trunk/Menu.aspx.cs
+++ trunk/Menu.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WcfServiceReference;
 
 public partial class Menu : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void MenuSearchButton_Click(object sender, EventArgs e)
+    {
+        rub_bbqEntities entities = new rub_bbqEntities(new Uri("http://localhost:1045/project/WcfDataService.svc"));
+        MenuItemGridView.DataSource = entities.MenuItems.Where(x => x.Name == MenuSearchTextBox.Text).ToList();
+        MenuItemGridView.DataSourceID = null;
+        MenuItemGridView.DataBind();
+    }
+    protected void MenuDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        MenuItemGridView.DataSource = null;
+        MenuItemGridView.DataSourceID = "MenuItemSqlDataSource";
     }
 }
