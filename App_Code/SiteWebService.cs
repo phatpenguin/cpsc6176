dba15e8f2e7d1f7ad25bc9d16e0bc0f811851cf7
kdod@msn.com <kdod@msn.com@6fac47d7-2e1d-cd27-4c29-69c2a8499821>
2012-07-27 02:50:43 +0000

    Login page and add new account page created. XML and XLST implemented oin Admins.aspx page

--- App_Code/SiteWebService.cs
+++ App_Code/SiteWebService.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 /// <summary>
 /// Summary description for SiteWebService
 /// </summary>
-[WebService(Namespace = "http://tempuri.org/")]
+[WebService(Namespace = "http://CPSC6176GroupC.edu/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
 // [System.Web.Script.Services.ScriptService]
@@ -20,8 +23,14 @@ public class SiteWebService : System.Web.Services.WebService {
     }
 
     [WebMethod]
-    public string HelloWorld() {
-        return "Hello World";
+    public DataSet GetCustomerList() {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["LocalSqlServer"].ConnectionString);
+        string cmdString = "Select * from [6176_su12_TeamC].dbo.Customers";
+        SqlDataAdapter dataAdaptor = new SqlDataAdapter(cmdString,conn);
+        DataSet CustomerDataSet = new DataSet();
+        dataAdaptor.Fill(CustomerDataSet,"Customers");
+        return CustomerDataSet;
+
     }
     
 }
---
+++ admin/admins.xslt
@@ -0,0 +1,22 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl">
+    <xsl:output method="xml" indent="yes"/>
+
+    <xsl:template match= "/">
+        <h3>List of Admins</h3>
+        <table border="1">
+            <tr>
+                <th>First Name</th><th>Last Name </th>          
+            </tr>
+            <xsl:apply-templates select="//admin" />
+        </table>
+    </xsl:template>
+    <xsl:template match="admin">
+        <tr>
+            <td><xsl:value-of select="FirstName"/></td>
+            <td><xsl:value-of select="LastName"/></td>
+        </tr>
+       
+    </xsl:template>
+</xsl:stylesheet>
